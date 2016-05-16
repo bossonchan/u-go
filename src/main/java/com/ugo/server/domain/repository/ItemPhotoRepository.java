6d6a094842e5c81f34adfc93b9d58cb4fe352b7c
陈柏信 <bossonchan@gmail.com>
2016-05-16 19:49:44 +0800

    update .gitignore

--- src/main/java/com/ugo/server/domain/repository/ItemPhotoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ugo.server.domain.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ugo.server.domain.entity.ItemPhoto;
-
-public interface ItemPhotoRepository extends JpaRepository<ItemPhoto, Long>{
-
-}
